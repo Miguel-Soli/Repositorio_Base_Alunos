84984938940b8ffe763b8d6a224b48387f4ae69d
Miguel <miguelthor209@gmail.com>
2025-09-12 14:09:08 -0300

    Add files via upload

---
+++ Modulo 1/Extra/Git.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.press('win')
+pyautogui.sleep(1)
+pyautogui.write('Google')
+pyautogui.sleep(1)
+pyautogui.press('enter')
+pyautogui.sleep(1)
+pyautogui.click(789,557)
+pyautogui.sleep(1)
+pyautogui.write('https://github.com/Miguel-Soli/Miguel-Soli')
+pyautogui.sleep(1)
+pyautogui.press('enter')
