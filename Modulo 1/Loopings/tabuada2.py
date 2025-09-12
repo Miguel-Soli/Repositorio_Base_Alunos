71094c2233013245d69ecfc6fd5e05f6f9212d5e
Miguel <miguelthor209@gmail.com>
2025-09-12 14:07:41 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/tabuada2.py
@@ -0,0 +1,9 @@
+while True:
+    numero = int(input("Digite o número: "))
+    i = int(input("Digite o número que deve começar: "))
+    final = int(input("Até onde a tabuada vai: "))
+    sair = input("sair do programa? Y/N: ")
+    while i <= final:
+        print(f"{i} X {numero} = {i * numero}")
+        i += 1  
+
